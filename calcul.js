bbc85e150096e96bb70be74e6858f4468501cc77
ayomikun286 <edegbaiaypmikun@gmail.com>
2025-04-29 15:57:12 +0100

    update

--- calcul.js
+++ calcul.js
@@ -1,17 +1,9 @@
 const display = document.getElementById("display");
-function append(value) {
-    display.value += value;
-}
-function clearDisplay() {
-    display.value = '';
-  }
-  function deleteLast() {
-    display.value = display.value.slice(0, -1);
-  }
-  function equal() {
-    try {
-      display.value = eval(display.value);
-    } catch {
-      display.value = 'Error';
-    }
-  }
+const append=(value) => display.value += value;
+const clearDisplay=() => display.value = '';
+const deleteLast=() => display.value = display.value.slice(0, -1);
+const equal=() =>display.value = eval(display.value);
+const sin = () => display.value = Math.sin(display.value);
+const cos = () => display.value = Math.cos(display.value);
+const tan = () => display.value = Math.tan(display.value);
+const squr = () => display.value = Math.sqrt(display.value);
