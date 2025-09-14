91afe447f68394bbbd875fab2f4f3df44e09b7b8
AYOMIKUN <edegbaiayomikun@gmail.com>
2025-09-14 10:28:21 +0100

    first commit

--- calcul.js
+++ calcul.js
@@ -2,7 +2,7 @@ const display = document.getElementById("display");
 const append=(value) => display.value += value;
 const clearDisplay=() => display.value = '';
 const deleteLast=() => display.value = display.value.slice(0, -1);
-const equal=() =>display.value = eval(display.value);
+const equal=() => display.value = eval(display.value);
 const sin = () => display.value = Math.sin(display.value);
 const cos = () => display.value = Math.cos(display.value);
 const tan = () => display.value = Math.tan(display.value);
